cee158643cd45f826792bb6405bd061980e2510a
nrementeria <nrementeria@medallia.com>
2021-06-05 20:29:37 -0300

    Added dockerhub practice

--- k8s-hands-on/backend/src/main.go
+++ k8s-hands-on/backend/src/main.go
@@ -14,6 +14,11 @@ type HandsOn struct {
 
 func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
+    if r.URL.Path != "/"{
+        http.NotFound(w, r)
+        return 
+    }
+
     resp := HandsOn{
         Time: time.Now(),
         Hostname: os.Getenv("HOSTNAME"),
