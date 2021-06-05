b8584814516abf56ea90fb28cdd6b87f8ea7ba5e
nrementeria <nrementeria@medallia.com>
2021-06-05 15:42:38 -0300

    Created basic api rest golang

---
+++ k8s-hands-on/src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "net/http"
+    "os"
+    "time"
+    "encoding/json"
+)
+
+type HandsOn struct {
+    Time time.Time `json:"time"`
+    Hostname string `json:"hostname"`
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+    resp := HandsOn{
+        Time: time.Now(),
+        Hostname: os.Getenv("HOSTNAME"),
+    }
+    jsonResp, err := json.Marshal(&resp)
+    if err != nil {
+        w.Write([]byte("Error"))
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonResp)
+}
+
+func main() {
+    http.HandleFunc("/", ServeHTTP)
+    http.ListenAndServe(":9090", nil)
+}
