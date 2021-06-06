b0541cee33c9feaa34f237bd66c26a1f83e3c5af
nrementeria <nrementeria@medallia.com>
2021-06-06 18:03:03 -0300

    Frontend service

--- k8s-hands-on/backend/src/main.go
+++ k8s-hands-on/backend/src/main.go
@@ -29,8 +29,10 @@ func ServeHTTP(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    w.WriteHeader(http.StatusOK)
+    //w.WriteHeader(http.StatusOK)
+    
     w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
     w.Write(jsonResp)
 }
 
